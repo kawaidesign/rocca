65764432a3eb81f3958f171fec5cfbd4c6be01f0
takayakawai <takuyakawai.1989@gmail.com>
2021-11-05 23:25:41 +0900

    1st commit

---
+++ script.js
@@ -0,0 +1,8 @@
+var mySwiper = new Swiper ('.swiper-container', {
+    effect: "fade",
+    loop: true,
+    speed: 1000,
+    pagination: '.swiper-pagination',
+    nextButton: '.swiper-button-next',
+    prevButton: '.swiper-button-prev',
+  })
